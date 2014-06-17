39c778e9fba8b4534c4ddf51a66fcfdb888e7dec
Nayana Hettiarachchi <nayana@corp-gems.com>
2014-06-17 15:56:32 +0700

    init

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PDO handler function 
+ */
+
+function getDbHandler()
+{
+	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+	$dbo = new PDO("mysql:host=localhost;dbname=cunm", "root", "", $options);
+	$dbo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $dbo;
+}
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+session_start();
+require_once('config.php');
+require_once 'vendor/autoload.php';
+require_once('db.php');
+require_once('smarty3/Smarty.class.php');
+
+$app = new \Slim\Slim();
+
+$smarty = new Smarty();
+$smarty->setTemplateDir('templates/');
+$smarty->setCompileDir('templates_c/');
+$smarty->setConfigDir('configs/');
+$smarty->setCacheDir('cache/');
+
+$app->get('/aboutus', function () use ($app, $smarty) {
+	$smarty->display('aboutus.tpl');
+});
+
+$app->get('/faq', function () use ($app, $smarty) {
+	$smarty->display('faq.tpl');
+});
+
+$app->get('/privacy', function () use ($app, $smarty) {
+	$smarty->display('privacy.tpl');
+});
+
+$app->get('/terms', function () use ($app, $smarty) {
+	$smarty->display('terms.tpl');
+});
+
+$app->get('/video', function () use ($app, $smarty) {
+	$smarty->display('video.tpl');
+});
+
+
+$app->get('/index2', function () use ($app, $smarty) {
+	$smarty->display('index2.tpl');
+});
+
+$app->get('/login', function () use ($app, $smarty) {
+	$smarty->display('login.tpl');
+});
+
+$app->get('/', function () use ($app, $smarty) {
+	$smarty->display('index.tpl');
+});
+
+$app->notFound(function () use ($app) {
+	$app->redirect(APP_PATH . "/");
+});
+
+$app->run();
